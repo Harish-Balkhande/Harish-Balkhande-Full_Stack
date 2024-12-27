5842801f11201d201ec993124e7df16d26a646d0
harish <harishbalkhande@gmail.com>
2024-12-27 19:12:13 +0530

    UI design of admin and user page completed

--- client/src/Components/User/Home.jsx
+++ client/src/Components/User/Home.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
+import CurrentOverStatus from '../sections/CurrentOverSatus';
+import ScoreBoard from '../sections/ScoreBoard';
 
 export default function Home() {
+  const [score, setScore] = useState([1, 3, 4, 2, 6, 'out']);
+
   return (
-    <div>Home</div>
+    <div>
+      <ScoreBoard />
+      <div style={{width:'50%', margin:"20px auto",textAlign:"center", border:"2px solid yellow",padding:"15px", boxShadow: "0 0 15px rgba(219, 205, 52, 0.66)" }}>
+        Animation Of Current Ball
+      </div>
+      <CurrentOverStatus ballScores={score} currentBallIndex="none" />
+    </div>
   )
 }
--- client/src/Components/sections/OverListingSection.jsx
+++ client/src/Components/sections/OverListingSection.jsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import CurrentOverStatus from './CurrentOverSatus';
 
 export default function OverListingSection() {
+
+  const [scoreInEachOver, setScoreInEachOver] = useState([
+    [1, 3, 4, 2, 6, 'out'],
+    [2, 2, 6, 3, 1, 'out'],
+    [4, 6, 3, 1, 2, 'out'],
+    ['out', 1, 4, 6, 3, 2],
+    [3, 6, 1, 4, 2, 'out'],
+    [2, 1, 'out', 3, 6, 4],
+  ]);
+
+  useEffect(() => {
+    // fetch();
+  }, []);
+
+  const fetch = async () => {
+    const response = await axios.get("url");
+    if (response.data) {
+      setScoreInEachOver(response.data);
+    }
+  };
+
   return (
-    <div>OverListingSection</div>
-  )
+    <div>
+      {scoreInEachOver.map((item, index) => (
+        <div key={index}>
+          <p style={{paddingLeft:"50px",margin:"0px",color:"aliceblue"}}>Over {index + 1}</p> 
+          <CurrentOverStatus ballScores={item} currentBallIndex="none" />
+        </div>
+      ))}
+    </div>
+  );
 }
